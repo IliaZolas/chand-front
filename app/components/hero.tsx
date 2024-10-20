a69db5ec464b0731f6610c004b1bab4269570d9c
Ilia Zolas <ilia@thedmcsa.co.za>
2024-10-20 14:35:59 +0200

    video

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import Image from 'next/image';
+import Image from 'next/image'
+import { VideoHTMLAttributes } from 'react';
 import GlitchEffect from './GlitchEffect';
 import HeroImage from '../../public/images/group-shots/test1.png';
 import { FaChevronDown } from 'react-icons/fa';
+// import HeroMovie from '../../public/video/video-test.mp4'; 
 
 const Hero: React.FC = () => {
     return (
         <div className="relative w-full h-screen flex flex-col justify-center items-center pt-20">
-            {/* Background Hero Image */}
+            {/* Background Hero Image
             <Image
                 src={HeroImage}
                 alt="Hero Image"
@@ -15,7 +17,21 @@ const Hero: React.FC = () => {
                 objectFit="cover" // Ensures the image covers the entire container
                 quality={100} // Set image quality
                 priority // Ensures the hero image loads faster
-            />
+            /> */}
+
+        <video
+            className="absolute top-0 left-0 w-full h-full object-cover"
+            autoPlay
+            loop
+            muted
+            playsInline // Ensures the video plays smoothly on mobile browsers
+            >
+            <source 
+                src="/video/video-test.mp4" 
+                type="video/mp4" 
+                />
+            Your browser does not support the video tag.
+        </video>
 
             {/* Glitch Effect */}
             <div className="absolute top-1/2 transform -translate-y-1/2">
